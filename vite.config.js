a69887eb9fef41bee9eb1fef18a3e02238d59a80
jungtop <jungtop@jungtops-MacBook-Pro.local>
2024-06-14 11:38:09 -0400

    upated tailwind config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base:"/10zintopjor.github.io/",
   plugins: [
     remix({
+      basename : "/10zintopjor.github.io/",
       ssr: false,
       future: {
         v3_fetcherPersist: true,
